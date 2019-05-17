00eb9058ab50f3ca970fc6432725653a3fc7c009
Ronlee12355 <Ronlee12355@outlook.com>
2019-05-17 23:17:53 +0800

    finish ranking loss and average precision

--- AveragePrecision.R
+++ AveragePrecision.R
@@ -3,19 +3,20 @@
 # predictions: Matrix of probabilities predicted by a classifier.
 
 AveragePrecision<-function(true_labels, predictions){
-  if(nrow(true_labels) != nrow(predictions) | ncol(true_labels) != ncol(predictions)){
-    stop("labels and predictions should be in the same format")
-  }
-  result = 0
-  M=ncol(true_labels)
-  N=nrow(true_labels)
-  
-  result<-sapply(seq(nrow(true_labels)), function(x){
-    tmp<-rbind(true_labels[x,], predictions[x,])
-    tmp<-tmp[,order(tmp[2,],decreasing = T)]
-    res<-tmp[1,][1]
-    
-    return(res)
-  })
-  return(sum(result)/N)
+  return(mldr::average_precision(true_labels, predictions))
+  # if(nrow(true_labels) != nrow(predictions) | ncol(true_labels) != ncol(predictions)){
+  #   stop("labels and predictions should be in the same format")
+  # }
+  # result = 0
+  # M=ncol(true_labels)
+  # N=nrow(true_labels)
+  # 
+  # result<-sapply(seq(nrow(true_labels)), function(x){
+  #   tmp<-rbind(true_labels[x,], predictions[x,])
+  #   tmp<-tmp[,order(tmp[2,],decreasing = T)]
+  #   res<-tmp[1,][1]
+  #   
+  #   return(res)
+  # })
+  # return(sum(result)/N)
 }
